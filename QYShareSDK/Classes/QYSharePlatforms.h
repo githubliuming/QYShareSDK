ca9a23200cd9a683de4bf59ead9290219b34f83b
liuming <1969489778@qq.com>
2017-05-31 18:46:21 +0800

    提交分享平台名称类

---
+++ QYShareSDK/Classes/QYSharePlatforms.h
@@ -0,0 +1,23 @@
+//
+//  QYSharePlatforms.h
+//  QYShareSDK
+//
+//  Created by liuming on 2017/5/31.
+//  Copyright © 2017年 burning. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+extern NSString * SHARE_PLARTFORM_QQ;  ///<* QQ
+extern NSString * SHARE_PLARTFORM_WX;  ///<* 微信好友
+extern NSString * SHARE_PLARTFORM_WXLINE; ///<* 微信朋友圈
+extern NSString * SHARE_PLARTFORM_INS;     ///<* ins
+extern NSString * SHARE_PLARTFORM_FACEBOOK; ///<* facebook
+extern NSString * SHARE_PLARTFORM_MESSAGER; ///<* messager
+extern NSString * SHARE_PLARTFORM_SINAWB;   ///<* 新浪微博
+extern NSString * SHARE_PLARTFORM_TWITTER;  ///<* 推特
+extern NSString * SHARE_PLARTFORM_WHATAPP;  ///<* whatApp
+extern NSString * SHARE_PLARTFORM_MORE;     ///<* 系统更多
+
+void initPlartformsNames();
---
+++ QYShareSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYShareSDK
+//
+//  Created by liuming on 2017/5/31.
+//  Copyright © 2017年 burning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
